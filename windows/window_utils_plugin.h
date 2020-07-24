b6063fcff6ba8fb3be6d9b4e8644bf1002583b0d
Josh Nies <50473759+joshnies@users.noreply.github.com>
2020-07-23 17:34:48 -0700

    Extract class to .h file

--- windows/window_utils_plugin.h
+++ windows/window_utils_plugin.h
@@ -1,7 +1,18 @@
+#pragma once
+
 #ifndef PLUGINS_EXAMPLE_window_utils_WINDOWS_H_
 #define PLUGINS_EXAMPLE_window_utils_WINDOWS_H_
 
 #include <flutter_plugin_registrar.h>
+
+#include <windows.h>
+#include <VersionHelpers.h>
+#include <flutter/method_channel.h>
+#include <flutter/plugin_registrar.h>
+#include <flutter/standard_method_codec.h>
+#include <memory>
+#include <sstream>
+#include <unordered_map>
 
 #ifdef FLUTTER_PLUGIN_IMPL
 #define FLUTTER_PLUGIN_EXPORT __declspec(dllexport)
@@ -21,3 +32,36 @@ FLUTTER_PLUGIN_EXPORT void WindowUtilsPluginRegisterWithRegistrar(
 #endif
 
 #endif  // PLUGINS_EXAMPLE_window_utils_WINDOWS_H_
+
+class WindowUtilsPlugin : public flutter::Plugin
+{
+    RECT normalRect;
+    RECT premaxWindowRect;
+
+public:
+    static void RegisterWithRegistrar(flutter::PluginRegistrar *registrar);
+
+    // Creates a plugin that communicates on the given channel.
+    WindowUtilsPlugin(
+        std::unique_ptr<flutter::MethodChannel<flutter::EncodableValue>> channel);
+
+    virtual ~WindowUtilsPlugin();
+
+private:
+    // The MethodChannel used for communication with the Flutter engine.
+    std::unique_ptr<flutter::MethodChannel<flutter::EncodableValue>> channel_;
+
+    // Called when a method is called on |channel_|;
+    void HandleMethodCall(
+        const flutter::MethodCall<flutter::EncodableValue> &method_call,
+        std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
+
+    // Returns whether or not the window is currently maximized.
+    bool GetIsMaximized();
+
+    // Maximize window.
+    void MaximizeWindow();
+
+    // Unmaximize window.
+    void UnmaximizeWindow();
+};
